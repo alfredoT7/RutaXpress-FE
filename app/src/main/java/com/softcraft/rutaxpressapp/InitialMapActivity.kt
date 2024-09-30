b179f3d7ab04c95d9d57223901ff38cb927436f8
jhoelito123 <amercer732@gmail.com>
2024-09-30 11:16:16 -0400

    Create permission on screen and conditions

--- app/src/main/java/com/softcraft/rutaxpressapp/InitialMapActivity.kt
+++ app/src/main/java/com/softcraft/rutaxpressapp/InitialMapActivity.kt
@@ -1,15 +1,24 @@
 package com.softcraft.rutaxpressapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 
 class InitialMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    // Variable para el mapa
+    // Variables globales
     private lateinit var map: GoogleMap
+
+    companion object{
+        const val REQUEST_CODE_LOCATION =0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +33,24 @@ class InitialMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+    }
+
+    private fun isLocationPermissionGranted() =
+        ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    //nos garantizamos de tener el permiso
+    private fun enableLocation(){
+        if(!::map.isInitialized) return
+        if (isLocationPermissionGranted()){
+            map.isMyLocationEnabled = true
+        }else{
+            requestLocationPermission()
+        }
+    }
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+             Toast.makeText(this, "Acepta los permisos de localizacion", Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION )
+        }
     }
 }
