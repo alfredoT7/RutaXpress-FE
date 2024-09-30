ba8365d66628a6c70b79213a88edbf23262c3b72
jhoelito123 <amercer732@gmail.com>
2024-09-30 10:49:24 -0400

    Feat: add maps

--- app/src/main/java/com/softcraft/rutaxpressapp/InitialMapActivity.kt
+++ app/src/main/java/com/softcraft/rutaxpressapp/InitialMapActivity.kt
@@ -1,12 +1,28 @@
 package com.softcraft.rutaxpressapp
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class InitialMapActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class InitialMapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    // Variable para el mapa
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.initial_map)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this) //implementamos OnReadyCallback
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
