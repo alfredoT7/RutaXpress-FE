550445d9ccd5ba3d87175f4b4acf07cd46b0746d
alfredoT7 <123456alfredotorri@gmail.com>
2024-10-24 22:11:23 -0400

    items added for recicle card view trufi

--- app/src/main/java/com/softcraft/rutaxpressapp/LineasFilterActivity.kt
+++ app/src/main/java/com/softcraft/rutaxpressapp/LineasFilterActivity.kt
@@ -3,12 +3,34 @@ package com.softcraft.rutaxpressapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.softcraft.rutaxpressapp.lineas.LineasAdapter
 
 class LineasFilterActivity : AppCompatActivity() {
+    private lateinit var rvLineas: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_lineas_filter)
-
+        initComponents()
+        initUI()
+        initListeners()
     }
+
+    private fun initComponents() {
+        rvLineas = findViewById(R.id.rvLineas)
+    }
+
+    private fun initUI() {
+        rvLineas.layoutManager = LinearLayoutManager(this)
+        rvLineas.adapter = LineasAdapter()
+    }
+
+    private fun initListeners() {
+        TODO("Not yet implemented")
+    }
+
+
 }
---
+++ app/src/main/java/com/softcraft/rutaxpressapp/lineas/LineasAdapter.kt
@@ -0,0 +1,4 @@
+package com.softcraft.rutaxpressapp.lineas
+
+class LineasAdapter {
+}
